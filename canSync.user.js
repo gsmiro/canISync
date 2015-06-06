33bb1620018540451261621ac681a476dc9862e3
Gabriel Seralha Miró <gsmiro@gmail.com>
2015-06-06 20:52:26 -0300

    Added functionality to check any link in the page

--- canSync.user.js
+++ canSync.user.js
@@ -8,25 +8,80 @@
 // @grant        none
 // ==/UserScript==
 
-var sync = document.getElementById('linkSynchronize');
-if(sync && sync.href){
-    var disp = sync.getElementsByClassName('canSync');
-    if(!disp || !disp.length){
-        var stat = sync.appendChild(document.createElement('span'));
-        stat.setAttribute('class','label canSync');
-        stat.setAttribute('id','canSync');
+//window.onload = function(){
+    function cook(str,fl){
+        var ret = {};
+        var found = false;
+        var parts = str.split('; ');
+        for(var idx in parts ){
+          var pt = parts[idx].split('=');
+          if(pt.length == 2 && (fl === undefined || fl.indexOf(pt[0]) > -1)){
+              ret[pt[0]] = pt[1];
+              found = true;
+          }
+        }
+        if(found)
+          return ret;
     }
-    if(typeof XMLHttpRequest === 'function'){
-        var req = new XMLHttpRequest();
-        req.onload = function(st){
-            if(req.status == 200){
-                stat.innerHTML = ';D';
-            }else{
-                stat.innerHTML = ':(';
+
+    function send(url,method,load){
+        if(typeof XMLHttpRequest === 'function'){
+            var req = new XMLHttpRequest();
+            req.onload = function(){
+                load(req);
+            }
+            req.open(method,url,true);
+            req.send();
+        }
+
+    }
+
+    var sync = document.getElementById('linkSynchronize');
+    if(sync && sync.href){
+        var disp = sync.getElementsByClassName('canSync');
+        if(!disp || !disp.length){
+            var stat = sync.appendChild(document.createElement('span'));
+            stat.setAttribute('class','label canSync');
+            stat.setAttribute('id','canSync');
+            stat.innerHTML = '...';
+        }
+        send(sync.href,'get',function(req){
+          if(req.status == 200){
+              stat.innerHTML = ';D';
+          }else{
+              stat.innerHTML = ':(';
+          }
+        })
+    }
+    var ck = cook(document.cookie,['locale','synclocale','lib','synclib','rsconf','syncrsconf']);
+    if(ck){
+        var orgn = document.location.origin
+        if(orgn){
+            parts = document.getElementsByTagName('a');
+            function proc(a){
+                if(a.href && a.href.indexOf(orgn+'/'+ck.locale+'/wol/d/') === 0){
+                    var qry = a.href.split('?');
+                    var url = qry[0].replace('/d/','/sydp/'+ck.rsconf+'/'+ck.lib+'/'+ck.syncrsconf+'/'+ck.synclib+'/')+'/%7Bno%7D';
+                    if(qry.length > 1)
+                        url = url + '?' + qry[1];
+                    if(url != a.href){
+                        send(url,'get',function(req){
+                            if(req.status === 200){
+                                console.log(url)
+                                a.innerHTML = a.innerHTML + ' ;D';                          
+                            }else {
+                                console.log(a);
+                                console.log(a.href)
+                                
+                            }
+                        })
+                    }
+                }
+
+            }
+            for(var idx = 0;idx < parts.length;idx++){
+                proc(parts[idx]);
             }
         }
-        console.log('Opening %s',sync.href);
-        req.open('get',sync.href,true);
-        req.send();
     }
-}
+//}
